34ed6632a0803ed0871ce872b616105fb5de094e
Ethan H. Jurman <ehj2229@rit.edu>
2015-05-18 19:04:45 -0400

    done? just need polish

--- js/game.js
+++ js/game.js
@@ -1,40 +1,96 @@
-var round = 30;
+var round = 3;
 var seed = '';
-var values = [];
+var values = -1;
+var gameover = false;
+var listen = false;
+var timer = 100;
+var gameType = 0;
+var score = 0;
 
-// getting 100 numbers that aren't 0, making that the seed
-while (seed.length < 100) { seed = seed + (Math.random()*10+'').split('.').join('')}
+var gametimer = setInterval(function(){
+  if (listen) {
+    timer--;
+    document.querySelectorAll('.time')[gameType - 1].style.width = timer + "%";
+    if (timer < 0){
+      gameover = true;
+      listen = false;
+    }
+  }
+  if (gameover){
+    document.getElementById('audioOver').play();
+    window.clearInterval(gametimer);
+  }
+},100);
 
 // starting the game
 var startGame = function(type){
+  gameType = type;
   document.getElementById('select-mode').style.display = 'none';
   var tableElement = document.getElementById('game-mode-'+type);
   tableElement.style.display = '';
 
-  selectElement(type, seed, 0)
-
+  while (seed.length < 100){
+    seed = seed + ((Math.random() * (type == 1 ? 3 : 8) + 1).toFixed());
+  }
+  console.log(seed)
+  startRound(type, 0)
 }
 
-var selectElement = function(type, values, index){
-  var time = 50000 / values.length;
+var startRound = function(type, index){
+  var time = 300;
   setTimeout(function(){
-    var ele = getTableElement(type, values[index]);
+    var ele = getTableElement(type, seed[index]);
     ele.style.background = 'lightblue';
-    ele.style.textShadow = '0px 0px 0px black';
+    ele.style.fontSize = '150px';
+    playAudio(seed[index]);
     setTimeout(function(){
-      var ele = getTableElement(type, values[index]);
+      var ele = getTableElement(type, seed[index]);
       ele.style.background = '';
-      ele.style.textShadow = '';
-      if (0 < round){
-        console.log(round--);
-        selectElement(type, values, ++index);
+      ele.style.fontSize = '';
+      if (index < round-1){
+        startRound(type, ++index);
+      } else {
+        listen = true;
       }
     }, time);
-  }, (time) * index);
+  }, time);
 }
 
 var getTableElement = function(type,number){
-  number = type == 1 ? Math.round(number / 3) : number;
-  number = type == 1 ? number : Number(number) + 3;
-  return document.getElementsByTagName('td')[number];
+  return document.querySelectorAll('td[data-value="' + number + '"]')[number > 4 ? 0 : type-1];
+}
+
+var clickValue = function(e, type){
+  var target = e.target || event.srcElement;
+  if (listen) {
+    timer = Math.min(100,timer + 5);
+    playAudio(target.getAttribute('data-value'));
+    if (seed[++values] != target.getAttribute('data-value')){
+      timer = -10;
+    }
+    console.log(values == round)
+    if (values == round-1){
+      round++;
+      values = -1;
+      listen = false;
+      score = score + timer;
+      document.querySelectorAll('.score')[gameType - 1].innerHTML = round - 3 + " : " + score;
+      setTimeout(function(){
+        startRound(type, 0);
+      },500);
+    }
+  }
+}
+
+var playAudio = function(value){
+  var audio = document.getElementById('audio' + value);
+  audio.play();
+  stopAudio(audio);
+}
+
+var stopAudio = function(audio){
+  setTimeout(function(){
+    audio.pause();
+    audio.currentTime = 0;
+  }, 300);
 }
